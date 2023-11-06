bdf7cc818c904686d6794b00b6b4a80603019309
not-a-thingy <76759140+not-a-thingy@users.noreply.github.com>
2023-11-06 12:28:20 +0800

    update test

--- main.dart
+++ main.dart
@@ -100,9 +100,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // axis because Columns are vertical (the cross axis would be
           // horizontal).
           //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
+          //sample test line
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
